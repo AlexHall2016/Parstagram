5127ca1a304f8fa868c3182e4c374169e73767f4
AlexanderHall <alexander.hal96@gmail.com>
2019-03-10 23:20:32 -0700

    saving the post

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
@@ -36,6 +37,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UI
     }
     
     @IBAction func SubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground {(success,error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            }else{
+                print("failed")
+            }
+            
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
